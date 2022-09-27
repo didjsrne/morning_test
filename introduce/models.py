8d3433cc6540535e3567f1af8051f6f2db3732e6
didjsrne <hyunjae0723@gmail.com>
2022-09-27 09:47:49 +0900

    Accesslog 구현해봄

--- introduce/models.py
+++ introduce/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class AccessLog(models.Model):
+    class Meta:
+        db_table = "accesslog"
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    location = models.CharField(max_length=256)
--- introduce/views.py
+++ introduce/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import AccessLog
 
-def introduce(request):
-    if request.method =='GET':
-        return render(request, 'introduce/introduce.html')
 # Create your views here.
+def introduce(request):
+    if request.method == 'GET':
+        user = request.user
+        access = AccessLog()
+        access.author = user
+        access.location = 'introduce'
+        access.created_at = AccessLog.created_at
+        access.save()
+        return render(request, 'introduce/introduce.html')
